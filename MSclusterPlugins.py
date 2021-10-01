b58ba575bf9397add68abdb378bc00b643fa6c0f
Ryan <ryoung@archer.feldmach.brandeis.edu>
2021-10-01 18:19:10 -0400

    Initial commit

---
+++ MSclusterPlugins.py
@@ -0,0 +1,164 @@
+# Extension that adds mountainsort cluster metrics to PHY for curation
+#
+# Installation:
+# Add this file to ~/.phy/plugins/
+#
+# Then open ~/.phy/phy_config.py and add this line:
+# c.TemplateGUI.plugins = ['MountainsortCurationJSONPlugin']
+
+import numpy as np
+from phy import IPlugin
+import json
+import os
+import pandas as pd
+
+class MSCurationTagsPlugin(IPlugin):
+    '''
+    Adds mountainsort curation tags statically to phy
+    table at the beginning of execution.
+
+    TODO
+    ----
+    Dynamically recompute metrics when clusters merge or split
+    '''
+
+    def __init__(self, remove_redundant_labels=True, ryan_preferred=True):
+        self.remove_redundant_labels = remove_redundant_labels
+        self.ryan_preferred = ryan_preferred
+
+    def attach_to_controller(self, controller):
+        """Note that this function is called at initialization time, *before*
+        the supervisor is created. The `controller.cluster_metrics` items are
+        then passed to the supervisor when constructing it."""
+
+        self.get_metric_table(controller=controller)
+        metrics = self.list_metric_names(remove_redundant_labels=self.remove_redundant_labels, ryan_preferred=self.ryan_preferred)
+        metrics = self.order_metric_names(metrics)
+        # Use this dictionary to define custom cluster metrics.  We memcache
+        # the function so that cluster metrics are only computed once and saved
+        # within the session, and also between sessions (the memcached values
+        # are also saved on disk).
+        for metric in metrics:
+            #controller.cluster_metrics[metric] = \
+            #    controller.context.memcache(get_cluster_lambda(metric, obj=self.df))
+            controller.cluster_metrics[('ms\n' + metric).replace('_','\n')] = \
+                self.get_cluster_lambda(metric)
+
+    def get_metric_table(self, json_filename=None, controller=None,
+                         adjust_index_by_order=True):
+        '''
+        Parse a json to obtain cluster metric table
+        '''
+
+        if json_filename is None:
+            # If no json, look a folder above for the metrics_tagged file
+            directory = os.path.expanduser(controller.dir_path)
+            directory = os.path.split(directory)[:-1]
+            directory = os.path.join(*directory)
+            json_filename =  os.path.join(directory, "metrics_tagged.json")
+        if not os.path.exists(json_filename):
+            raise FileExistsError(f"{json_filename} does not exist!")
+
+        with open(json_filename,'r') as F:
+            J = json.load(F)
+
+        clusters = J['clusters']
+
+        df = pd.DataFrame()
+        for i, elem in enumerate(clusters):
+
+            label = elem['label']
+            metrics = pd.DataFrame.from_dict(elem['metrics'], orient='index').T
+            metrics.index = pd.Index([label], name='id')
+            if len(elem['tags']) == 0:
+                tags = ''
+            else:
+                tags = str(elem['tags'] if len(elem['tags'])!=1 else elem['tags'][0])
+            metrics = metrics.assign(tags=tags)
+
+            if i > 0:
+                df = df.append(metrics)
+            else:
+                df = metrics
+
+        if adjust_index_by_order:
+
+            df.sort_index(inplace=True)
+            old_index = df.index
+            new_index = tuple(pd.Index(range(df.shape[0]),
+                              name="label"))
+            lookup = pd.Series(index=old_index, data=new_index)
+            lookup.loc[0] = -1
+            df.loc[:, 'overlap_cluster'] = lookup.loc[df.overlap_cluster.astype('int')].values
+            df.index = new_index
+
+        self.df = df
+
+    def get_cluster_metric(self, cluster_id:int, metric:str, obj=None):
+        '''
+        Obtain a metric at a cluster id
+        '''
+
+        if cluster_id in self.df.index:
+            return self.df.loc[cluster_id][metric]
+        else:
+            return np.nan
+
+
+    def get_cluster_lambda(self, metric:str):
+        '''
+        reeturna  lambda function that reports the value of a metric for a cluster id
+        '''
+
+        df = self.df[metric]
+
+        def func(cluster_id, df=df):
+            if cluster_id in df.index:
+                return df.loc[cluster_id]
+            else:
+                # TODO, if not found, then reccompute cluster metrics; user may
+                # have merged or split a cluster
+                return np.nan
+        return func
+
+    def list_metric_names(self, remove_redundant_labels=False, ryan_preferred=False):
+        metric_list = list(self.df.columns)
+        if remove_redundant_labels:
+            rlabels = ["num_events", "peak_amp", "firing_rate"]
+            metric_list = [metric for metric in metric_list
+                           if metric not in rlabels]
+        if remove_redundant_labels:
+            rlabels = ["num_events","peak_amp","firing_rate", "t1_sec", "t2_sec", "dur_sec"]
+            metric_list = [metric for metric in metric_list
+                           if metric not in rlabels]
+        return metric_list
+
+    def order_metric_names(self, metric_list):
+        """
+        This function merely imbues an order to the list of metrics
+        based on which I find most important to my clustering.
+        """
+
+        order = ["isolation", "peak_snr", "noise_overlap",
+                 "overalap_cluster", "bursting_parent", "tags"]
+        order = np.array(order)
+        mapnames = {name:np.where(order == name)[0] if name in order else np.nan
+                   for name in metric_list}
+        metric_list = sorted(metric_list, 
+                             key=lambda x: mapnames[x])
+        return metric_list
+
+class RyanReduceMetrics(IPlugin):
+    '''
+    Remove metrics from phy that I find useless for tetrodes
+    '''
+    from phy import IPlugin
+    pass
+
+class MSCurationSave(IPlugin):
+    '''
+    Whenever a save event is emitted, save the state of the cluster
+    tags to mountainsort files
+    '''
+    from phy import IPlugin
+    pass
---
+++ mountainsort_to_phy.py
@@ -0,0 +1,149 @@
+# ----------------------------------------------
+# Spikeinterface : Mountainsort curation via Phy
+# ----------------------------------------------
+import json, os, tqdm
+import spikeinterface.core.waveform_extractor as wave_extract
+import spikeinterface.exporters.to_phy as phy
+import spikeinterface.extractors.mdaextractors as mda_extract
+
+# -------------
+# Configuration
+# -------------
+config = {}
+config['filtered']        = True # Use filt.mda instead of raw.mda?
+config['toleratemissing'] = True # Throw an error for missing tetrodes? Or just skip...
+config['skipproc']        = True # Skip already processed folders?
+# Directory to look for tetrodes to send into Phy
+config['parent_path'] = '/Volumes/GenuDrive/RY16_direct/MountainSort/RY16_36.mountain/'
+
+# Store some data from the error
+error = {}
+error['missing'] = []
+error['incompletemda'] = []
+
+for folder in tqdm.tqdm(os.listdir(config['parent_path']), 
+                        desc="Process mountainsort folders"):
+
+    # Set the full path to the tetrdode folder
+    local_path = os.path.join(config['parent_path'], folder)
+
+    # The files that we may be using ...
+    phyplace    = local_path + os.path.sep + "phy"
+    prv_file    = local_path + os.path.sep + 'raw.mda.prv'
+    filt_file   = local_path + os.path.sep + 'filt.mda'
+    params_file = local_path + os.path.sep + 'params.json'
+    firings_file = local_path + os.path.sep + 'firings_raw.mda'
+    curated_firings_file = local_path + os.path.sep + 'firings_raw.mda'
+
+    # If skipproc and a phy foler already created, skip this folder
+    if config['skipproc'] and os.path.exists(os.path.join(phyplace, 'params.py')):
+        continue
+    if not os.path.isdir(local_path):
+        continue
+
+    print("Processing " + local_path)
+
+    # Derive the properties of the prv
+    with open(params_file, 'r') as F:
+        params_dict = json.load(F)
+
+    # Create the mda object
+    if config['filtered']:
+        if config['toleratemissing'] and not os.path.exists(filt_file):
+            print(f"Warning {local_path} has no filt.mda")
+            error['missing'].append(filt_file)
+            continue
+
+        not_complete = True
+        geom_empty   = False
+        print("Extracting MDA")
+        while not_complete and not geom_empty:
+
+            try:
+                mdafile = mda_extract.read_mda_recording(local_path,
+                                                         raw_fname=filt_file)
+                not_complete = False
+
+            # When an assertion error occurs, it's usually due to the geom.csv
+            # having more coordinates than there are channels in the *.mda. So
+            # we remove a line from the file (remove a coordinate) until the
+            # coordinate count matches. Janky, but it works.
+            except AssertionError:
+                geom_file = os.path.join(local_path, 'geom.csv')
+                with open(geom_file, 'r') as F:
+                    lines = F.readlines()
+                    lines = lines[:len(lines)-1]
+                    if len(lines) == 0:
+                        print("geom_file is empty!")
+                        geom_empty = True
+                        continue
+                with open(geom_file, 'w') as F:
+                    F.writelines(lines)
+
+        # If the geom file is empty, because it never converges to
+        # the proper number of channels, continue to the next tetrode
+        if geom_empty:
+            continue
+
+        # If we're using filt.mda, the file is already filtered
+        mdafile.annotate(is_filtered=True)
+
+    else:
+        with open(prv_file, 'r') as F:
+            json_dict = json.load(F)
+        original_path = json_dict['original_path']
+        mdafile = mda_extract.read_mda_recording(local_path,
+                                                 raw_fname=original_path)
+
+    # ----------------------------------------------
+    # Derive a file pointing to filtered spikes file
+    # ----------------------------------------------
+    print("Getting spikes file")
+    samprate = params_dict['samplerate']
+    spikefile = (
+        mda_extract.read_mda_sorting(firings_file,
+                                     sampling_frequency=samprate))
+
+    # Remove any folders and files that will hamper this process
+    print("Processing waveform")
+    waveform_file = local_path + os.path.sep + 'waveform' + os.path.sep + "PCA"
+    if os.path.exists(waveform_file):
+        [os.remove(os.path.join(waveform_file, f)) for f in
+         os.listdir(waveform_file)]
+        os.rmdir(waveform_file)
+    waveform_file = local_path + os.path.sep + 'waveform' + os.path.sep + "waveforms"
+    if os.path.exists(waveform_file):
+        [os.remove(os.path.join(waveform_file, f)) for f in
+         os.listdir(waveform_file)]
+        os.rmdir(waveform_file)
+    waveform_file = local_path + os.path.sep + 'waveform'
+    if os.path.exists(waveform_file):
+        [os.remove(os.path.join(waveform_file, f)) for f in
+         os.listdir(waveform_file)]
+        os.rmdir(waveform_file)
+
+    # ----------------------
+    # Extract spike waveform
+    # ----------------------
+    try:
+        waveform = wave_extract.extract_waveforms(mdafile, spikefile,
+                                                  waveform_file,
+                                                  max_spikes_per_unit=None,
+                                                  ms_before=3., 
+                                                  ms_after=4.,
+                                                  return_scaled=False, 
+                                                  n_jobs=10,
+                                                  total_memory='50M',
+                                                  progress_bar=True)
+    except ValueError:
+        error['incompletemda'] = (mdafile, spikefile, waveformfile)
+
+    # ----------------------------------
+    # Perform the actual export process
+    # ----------------------------------
+    print("Processing phy")
+    phyplace = local_path + os.path.sep + "phy"
+    if os.path.exists(phyplace):
+        [os.remove(os.path.join(phyplace, f)) for f in os.listdir(phyplace)]
+        os.rmdir(phyplace)
+    phyfiles = phy.export_to_phy(waveform, phyplace)
